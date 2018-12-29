fe82aa7bc8151486e2d0507ed769f29f2f8906a0
David Schneider <dnschneid@gmail.com>
2018-12-28 16:04:22 -0800

    Update HomeAssistant component

--- examples/mysmartblinds.py
+++ examples/mysmartblinds.py
@@ -3,7 +3,8 @@ HomeAssistant cover component for MySmartBlinds automated blind tilting
 
 Place in .homeassistant/custom_components/cover/
 
-Example configuration:
+Example configurations:
+# Explicit, bluetooth-only
 cover:
   - platform: mysmartblinds
     blinds:
@@ -11,6 +12,21 @@ cover:
         friendly_name: Living Room Blinds
         mac: 12:34:56:78:9A:BC
         access_token: abcdef123456
+# Implicit, uses cloud or bluetooth
+cover:
+  - platform: mysmartblinds
+    username: user
+    password: password
+# Explicit, uses cloud or bluetooth
+cover:
+  - platform: mysmartblinds
+    username: user
+    password: password
+    blinds:
+      livingroom:
+        friendly_name: Living Room Blinds
+        mac: 12:34:56:78:9A:BC
+        # Missing access tokens will be fetched from the cloud
 
 For more details about this platform, please refer to the documentation at
 https://github.com/dnschneid/pysmartblinds
@@ -26,10 +42,11 @@ from homeassistant.components.cover import (
     SUPPORT_OPEN_TILT, SUPPORT_CLOSE_TILT, SUPPORT_STOP_TILT,
     SUPPORT_SET_TILT_POSITION, ATTR_TILT_POSITION)
 from homeassistant.const import (
-    CONF_ACCESS_TOKEN, CONF_FRIENDLY_NAME, CONF_MAC)
+    CONF_ACCESS_TOKEN, CONF_FRIENDLY_NAME, CONF_MAC,
+    CONF_PASSWORD, CONF_USERNAME)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['pygatt==3.2.0', 'pexpect==4.6.0']
+REQUIREMENTS = ['pygatt==3.2.0', 'pexpect==4.6.0', 'smartblinds-client==0.2']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -44,12 +61,14 @@ TILT_FEATURES = (SUPPORT_OPEN_TILT | SUPPORT_CLOSE_TILT | SUPPORT_STOP_TILT |
 
 BLIND_SCHEMA = vol.Schema({
     vol.Required(CONF_MAC): cv.matches_regex(MAC_REGEX),
-    vol.Required(CONF_ACCESS_TOKEN): cv.matches_regex(TOKEN_REGEX),
+    vol.Optional(CONF_ACCESS_TOKEN): cv.matches_regex(TOKEN_REGEX),
     vol.Optional(CONF_FRIENDLY_NAME): cv.string,
 })
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_BLINDS): vol.Schema({cv.slug: BLIND_SCHEMA}),
+    vol.Optional(CONF_USERNAME): cv.string,
+    vol.Optional(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_BLINDS): vol.Schema({cv.slug: BLIND_SCHEMA}),
 })
 
 SERVICE_TO_METHOD[SERVICE_SET_COVER_TILT_POSITION]['schema'] = (
@@ -64,18 +83,46 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the blind."""
     blinds = []
 
-    for device, device_config in config[CONF_BLINDS].items():
+    sbc = None
+    username = config.get(CONF_USERNAME, '')
+    password = config.get(CONF_PASSWORD, '')
+    if username and password:
+        from smartblinds_client import SmartBlindsClient
+        sbc = SmartBlindsClient(username=username, password=password)
+        try:
+            sbc.login()
+        except Exception:
+            _LOGGER.error("Unable to log in to MySmartBlinds")
+
+    for device, device_config in config.get(CONF_BLINDS, {}).items():
         friendly_name = device_config.get(CONF_FRIENDLY_NAME, device)
         mac = device_config.get(CONF_MAC)[:17]
-        token = device_config.get(CONF_ACCESS_TOKEN)
+        token = device_config.get(CONF_ACCESS_TOKEN, None)
 
         blinds.append(
             CoverMySmartBlinds(
-                hass,
-                device, friendly_name,
+                hass, sbc,
+                friendly_name,
                 mac, token
             )
         )
+
+    if not blinds and sbc is not None:
+        try:
+            sbcBlinds, _ = sbc.get_blinds_and_rooms()
+        except Exception:
+            _LOGGER.error("Failed to fetch blinds from the cloud")
+            return False
+        for blind in sbcBlinds:
+            blinds.append(
+                CoverMySmartBlinds(
+                    hass, sbc,
+                    blind.name,
+                    ':'.join(('%02X' for x in blind.mac_address)),
+                    tuple(x for x in blind.passkey)
+                )
+            )
+
     if not blinds:
         _LOGGER.error("No blinds added")
         return False
@@ -89,12 +136,12 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class CoverMySmartBlinds(CoverDevice):
     """Representation of a MySmartBlinds cover."""
 
-    def __init__(self, hass, device_id, friendly_name, mac, access_token):
+    def __init__(self, hass, sbc, friendly_name, mac, access_token):
         """Initialize the MySmartBlinds cover."""
         from pysmartblinds import Blind
         self.hass = hass
         self._name = friendly_name
-        self._blind = Blind(mac, access_token)
+        self._blind = Blind(mac, key=access_token, smart_blinds_client=sbc)
         self._blind.pos(200)
         self._blind.set_callback(self.schedule_update_ha_state)
 
